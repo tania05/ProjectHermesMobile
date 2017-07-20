6057dfbe9baf4d064092753b448de8466553ae0f
Rahat Mahbub <rahatm1@uvic.ca>
2017-07-20 15:48:49 -0700

    New Message encoding

--- app/src/main/java/ca/projecthermes/projecthermes/Ethereum/Ethereum.java
+++ app/src/main/java/ca/projecthermes/projecthermes/Ethereum/Ethereum.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.util.Log;
 
 import org.ethereum.geth.Account;
+import org.ethereum.geth.BigInt;
+
+import java.math.BigInteger;
 
 import io.ethmobile.ethdroid.EthDroid;
 import io.ethmobile.ethdroid.KeyManager;
@@ -18,6 +21,9 @@ public class Ethereum {
     private Account account;
     private final String TAG = this.getClass().getSimpleName();
     private static Ethereum instance;
+    private final String SmartContractAddress = "0xAc7d48eb7Ca5bcd18a03c3C517EA1238D80D1cf4";
+    private final BigInt gasAmount = new BigInt(400000);
+    private final long msgCost = (long) 1e17;
 
     private Ethereum(Context context) {
         try {
@@ -37,7 +43,7 @@ public class Ethereum {
             eth.setMainAccountAtIndex(0);
             eth.start();
 
-            Log.d(TAG, "Address: " + account.getAddress().getHex());
+            Log.d(TAG, "Wallet Address: " + account.getAddress().getHex());
 
 
         } catch (Exception e) {
@@ -53,7 +59,31 @@ public class Ethereum {
         return instance;
     }
 
-    public void newMessage(String msgId, int privateNonce, int publicNonce) {}
+    public void newMessage(String msgId, int privateNonce, int publicNonce) {
+        try {
+            String encodedData = "0x324120650000000000000000000000000000000000000000000000000000000000000060"
+                    + String.format("%-64x", publicNonce).replace(' ', '0')
+                    + String.format("%-64x", privateNonce).replace(' ', '0')
+                    + String.format("%64x", 32).replace(' ', '0')
+                    + String.format("%32x", new BigInteger(1, msgId.replaceAll("-", "").getBytes("US-ASCII")));
+
+            keyManager.unlockAccount(account, "password"); //TODO: ask user for password
+            eth.newTransaction()
+                    .to(SmartContractAddress)
+                    .gasAmount(gasAmount)
+                    .value(msgCost)
+                    .data(encodedData)
+                    .send();
+            Log.e(TAG, "MSG UUID: " + msgId);
+            Log.e(TAG, "privateNonce : " + privateNonce);
+            Log.e(TAG, "publicNonce: " + publicNonce);
+
+            Log.e(TAG, "Encoded data: " + encodedData);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, "new Message failed");
+        }
+    }
     public void addHop(String msgId, int publicNonce) {}
     public void receiveMessage(String msgId, int privateNonce) {}
 
